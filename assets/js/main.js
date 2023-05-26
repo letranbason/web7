f1661e4857822435bb44a1771044c803b16f9ffc
letranbason <109140737+letranbason@users.noreply.github.com>
2023-05-26 18:41:55 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,13 @@
 var prevBtn = document.getElementById('prevBtn');
 var nextBtn = document.getElementById('nextBtn');
 
-App = {
+var prevBtn_event = document.getElementById('prevBtn_event');
+var nextBtn_event = document.getElementById('nextBtn_event');
+
+var prevBtn_top = document.getElementById('prevBtn_top');
+var nextBtn_top = document.getElementById('nextBtn_top');
+
+var App = {
   sliderTable: function() {
     jQuery('.slidertable').slick({
       dots: false,
@@ -13,8 +19,61 @@ App = {
       nextArrow: nextBtn, // Set custom next button
     });
   },
-};
 
-jQuery(document).ready(function() {
+  backGroundSlide: function() {
+    jQuery('.background-slide').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2500,
+      prevArrow: false,
+      nextArrow: false,
+    });
+  },
+
+  sliderGrid: function () {
+    jQuery('.slider-grid').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay:false,
+      autoplaySpeed: 2500,
+      prevArrow: prevBtn_event,
+      nextArrow: nextBtn_event,
+    })
+  },
+
+  sliderNews: function () {
+    jQuery('.sliderNews').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay:false,
+      autoplaySpeed: 2500,
+      prevArrow: prevBtn_top,
+      nextArrow: nextBtn_top,
+    })
+  },
+
+}
+
+document.addEventListener('DOMContentLoaded', function() {
   App.sliderTable();
+  App.backGroundSlide();
+  App.sliderGrid();
+  App.sliderNews();
+
+  const showMoreButtons = document.querySelectorAll('.show-more');
+  showMoreButtons.forEach(button => {
+    button.addEventListener('click', function() {
+      const content = button.parentNode.querySelector('.hidden-content');
+      const buttonText = button.innerText.trim();
+
+      if (content.style.display === 'none') {
+        content.style.display = 'block';
+        button.innerText = 'Ẩn Bớt';
+      } else {
+        content.style.display = 'none';
+        button.innerText = 'Xem Thêm';
+      }
+    });
+  });
 });
